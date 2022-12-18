981008d290eb7d377627f2ecb1b92e0c0e587fe4
hkobashi <71940017+hkobashi@users.noreply.github.com>
2022-12-18 13:57:07 +0900

    クラスター設定を追加

--- rds.tf
+++ rds.tf
@@ -41,3 +41,31 @@ resource "aws_db_parameter_group" "db_pmtgrp" {
 
 }
 
+# RDSクラスター設定
+resource "aws_rds_cluster" "aurora_clstr" {
+  cluster_identifier = "aurora-cluster"
+  # 初期作成されるスキーマ
+  database_name = "mydb"
+
+  # 管理ユーザー情報
+  master_username = "admin"
+  master_password = "1234Admin5678"
+  port = 3306 # 使用ポート
+  apply_immediately = false
+
+  skip_final_snapshot = true
+
+  engine = "aurora-mysql"
+  engine_version = "5.7.mysql_aurora.2.07.2"
+
+  # 使用するセキュリティグループを定義
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.db_subgrp.name
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.db_clstr_pmtgrp.name
+
+  tags = {
+    Name = "aurora-cluster"
+    Created_by = "Terraform"
+  }
+}
+
