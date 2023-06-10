6f0474200fb7f9e0a402b4f34f63bb03c6617aee
hkobashi <h.k21.0419@gmail.com>
2023-06-10 16:04:04 +0900

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -1,4 +1,5 @@
 # settings for certification
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
 resource "aws_acm_certificate" "cert" {
   domain_name = "discriminator.link"
   validation_method = "DNS"
